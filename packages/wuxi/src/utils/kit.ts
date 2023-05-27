eb9a34828859895f217406d46f36608d20dfb48f
gumingWu <15626185054@163.com>
2023-05-27 13:57:53 +0800

    feat: resolve path

--- packages/wuxi/src/utils/kit.ts
+++ packages/wuxi/src/utils/kit.ts
@@ -3,9 +3,18 @@ import { tryResolveModule, importModule } from "./esm";
 export async function loadKit(rootDir: string) {
   try {
     const localKit = await tryResolveModule('kit', rootDir)
-    const rootUrl = localKit ? rootDir : await tryResolveModule('wuxt') || rootDir
-    await importModule('kit', rootUrl)
+    const rootUrl = localKit ? rootDir : await tryResolveNuxt()
+    console.log(rootUrl)
+    // await importModule('kit', rootUrl)
   } catch(e) {
     throw new Error(e)
   }
+}
+
+async function tryResolveNuxt () {
+  for (const pkg of ['nuxt3']) {
+    const path = await tryResolveModule(pkg)
+    if (path) { return path }
+  }
+  return null
 }
